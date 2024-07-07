9247e23f5ef9fbef694f038ed9875be168774abd
agro40tech <volkovvova67@gmail.com>
2024-07-08 01:36:52 +0400

    chore: добавляет tailwindcss
    Добавляет tailwindcss, clsx, postcss, autoprefexier. Так же добавлены новые команды для работы с dev
    режимом

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App.tsx'
-import './index.css'
+import { App } from './app/App'
+
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'src/app/*.{html,ts,tsx}',
+		'src/components/**/*.{html,ts,tsx}',
+		'src/pages/**/*.{html,ts,tsx}',
+		'./index.html',
+	],
+	theme: {
+		extend: {},
+	},
+
+	plugins: [],
+}
