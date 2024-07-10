d16bb28f7eac2ba25b4197d9cd732c31a0c743aa
agro40tech <volkovvova67@gmail.com>
2024-07-10 14:41:08 +0400

    feat: добавляет иконки для приложения и api к ним

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+import ICON_ARROW_DOWN from './icon-arrow-down.svg'
+import ICON_BACK_ARROW from './icon-back-arrow.svg'
+import ICON_BLOCK_TEXT from './icon-block-text.svg'
+import ICON_CLOSE from './icon-close.svg'
+import ICON_CURVED_LINE from './icon-curved-line.svg'
+import ICON_DOTTED_LINE from './icon-dotted-line.svg'
+import ICON_GROUP from './icon-group.svg'
+import ICON_SETTINGS from './icon-settings.svg'
+import ICON_STRAIGHT_LINE from './icon-straight-line.svg'
+import ICON_TEXT from './icon-text.svg'
+
+export {
+	ICON_ARROW_DOWN,
+	ICON_BACK_ARROW,
+	ICON_BLOCK_TEXT,
+	ICON_CLOSE,
+	ICON_CURVED_LINE,
+	ICON_DOTTED_LINE,
+	ICON_GROUP,
+	ICON_SETTINGS,
+	ICON_STRAIGHT_LINE,
+	ICON_TEXT,
+}
